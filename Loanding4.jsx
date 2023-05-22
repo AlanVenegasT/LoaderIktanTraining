0095bbd9d74bad7e19c38e8ee4fc2a0cf848570c
AlanVenegasT <99163514+AlanVenegasT@users.noreply.github.com>
2023-05-22 13:40:53 -0600

    Loader responsive

---
+++ Loanding4.jsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import '../css/Loanding4.css';
+
+const Loanding4 = () => {
+    return (
+        <>
+
+            <body class='body'>
+            <div class='flex-direction: column;'>
+                <section class="sec-loading">
+                    <div class="one">
+                    </div>
+                </section>
+
+                <h1 class='typewrite h1'>Iktan Training.....</h1>
+            </div>
+            </body>
+
+        </>
+    )
+}
+
+export default Loanding4
